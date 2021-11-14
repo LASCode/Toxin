6b7647ad79f247ab9d33a59a73d0f6a0423c2e43
LASCode <geimixa123@gmail.com>
2021-11-14 19:41:11 +0300

    add pug and scss loaders to getLoaders module

--- webpack/loaders.js
+++ webpack/loaders.js
@@ -1,8 +1,21 @@
 const fs = require('fs'),
-      path = require('path')
+      path = require('path'),
+  MiniCssExtractPlugin = require('mini')
 
 const getLoaders = {
-
+  pug(){
+    return [
+      {loader: "pug-loader", options: { pretty: true }}
+    ]
+  },
+  scss(){
+    return [
+      MiniCssExtractPlugin.loader,
+      {loader: "css-loader", options: { sourceMap: true }},
+      {loader: "resolve-url-loader", options: {root: path.resolve(__dirname, '../')}},
+      {loader: "sass-loader", options: { sourceMap: true }}
+    ]
+  },
 }
 
 module.exports = getLoaders
