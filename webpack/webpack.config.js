2395aecb1086ef13a5cc607791b0d3eb57fefa90
LASCode <geimixa123@gmail.com>
2021-11-14 19:45:05 +0300

    update webpack.config. add pug,scss,fonts rules and correct entrypoints

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,13 +3,30 @@ const fs = require('fs'),
       getLoaders = require('./loaders'),
       createProjectManager = require('./projectFunc')
 
-
+const {PM_entries, PM_fun, PM_path} = createProjectManager(path.resolve(__dirname, '../'))
+console.log(PM_entries)
 module.exports = {
-  entry: {},
+  entry: PM_entries,
   output: {
     filename: "js/[name].js",
-    path: path.resolve(__dirname, 'dist')
+    path: PM_path.dist
   },
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: getLoaders.pug()
+      },
+      {
+        test: /\.(s*)css$/,
+        use: getLoaders.scss()
+      },
+      {
+        test: /(\.(woff2?|ttf|eot|otf)$|font.*\.svg$)/,
+        type: "asset/resource",
+        generator: {filename: 'fonts/[name][ext]'}
+      },
+    ]
+  },
   plugins: []
 }
