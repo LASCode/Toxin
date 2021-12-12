8318696daa3aa40217c2bd526bfce234ee6b8d38
LASCode <geimixa123@gmail.com>
2021-12-12 18:21:29 +0300

    create pagination component

---
+++ src/components/pagination/pagination.js
@@ -0,0 +1,16 @@
+import 'paginationjs'
+
+const container = $(".pagination")
+container.find(".pagination__wrapper").each((index, el)=>{
+  const totalPages = $(el).closest(".pagination").data('totalpages')
+
+  const pagesArr = new Array(totalPages).fill(false).map((el, i)=>i+1)
+  console.log(pagesArr)
+  $(el).pagination({
+    pageSize: 1,
+    dataSource: pagesArr,
+    pageRange: 1,
+    prevText: "arrow_back",
+    nextText: "arrow_forward"
+})
+})
