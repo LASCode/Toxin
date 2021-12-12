fe8582d536598a02c4f151a5988dc9f2b88bf48b
LASCode <geimixa123@gmail.com>
2021-12-12 18:26:49 +0300

    Update loaders file, add func thaht return css and less loaders array

--- webpack/loaders.js
+++ webpack/loaders.js
@@ -8,12 +8,27 @@ const getLoaders = {
       {loader: "pug-loader", options: { pretty: true }}
     ]
   },
+  css(){
+    return [
+      MiniCssExtractPlugin.loader,
+      {loader: "css-loader", options: { sourceMap: true }},
+      // {loader: "resolve-url-loader", options: {root: path.resolve(__dirname, '../')}},
+    ]
+  },
   scss(){
     return [
       MiniCssExtractPlugin.loader,
       {loader: "css-loader", options: { sourceMap: true }},
       {loader: "resolve-url-loader", options: {root: path.resolve(__dirname, '../')}},
       {loader: "sass-loader", options: { sourceMap: true }}
+    ]
+  },
+  less(){
+    return [
+      MiniCssExtractPlugin.loader,
+      {loader: "css-loader", options: { sourceMap: true }},
+      {loader: "resolve-url-loader", options: {root: path.resolve(__dirname, '../')}},
+      {loader: "less-loader", options: { sourceMap: true }}
     ]
   },
 }
