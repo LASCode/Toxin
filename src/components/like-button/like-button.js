de8e2279ec9d225b1f3138da62a835ce891f02d8
LASCode <geimixa123@gmail.com>
2021-12-12 18:13:59 +0300

    create like-button component

---
+++ src/components/like-button/like-button.js
@@ -0,0 +1,22 @@
+
+const onClick = (e)=>{
+  const target = e.target // Элемент, по которому произошел клик
+  const action = $(target).closest('*[data-action]').data('action') // Дата атрибут элемента, по которому произошел клик
+
+  if (action){
+    if (action==='like-it'){
+      const container = $(target).closest('.like-button')
+      const isLiked = container.hasClass("like-button--liked")
+      const counter = container.find(".like-button__count")
+      const count = Number(counter.html())
+      container.toggleClass("like-button--liked")
+      if (isLiked){
+        counter.html(count-1)
+      }else{
+        counter.html(count+1)
+      }
+    }
+  }
+}
+
+$(document).on("click", onClick)
