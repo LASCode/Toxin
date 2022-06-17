081b67d74f9259b86e2c400d1b03c8267e583ad3
LASCode <geimixa123@gmail.com>
2022-06-18 02:57:46 +0300

    create all cards components

---
+++ src/components/card-room/card-room.js
@@ -0,0 +1,11 @@
+import 'owl.carousel';
+
+$('.js-carousel1').owlCarousel({
+  items: 1,
+  nav: true,
+  rewind: true,
+  navText: [
+    '<span class="arrow-owl arrow-left">expand_more</span>',
+    '<span class="arrow-owl arrow-right">expand_more</span>',
+  ],
+});
