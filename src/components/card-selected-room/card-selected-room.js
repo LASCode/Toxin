17b961e81fd06a1ef850e601ea363506a2f89bfe
LASCode <geimixa123@gmail.com>
2022-10-22 05:19:41 +0300

    cards refactoring. add props default values and ability to set field values

--- src/components/card-selected-room/card-selected-room.js
+++ src/components/card-selected-room/card-selected-room.js
@@ -2,6 +2,7 @@ import './card-selected-room.scss';
 import '../button/button';
 import '../title/title';
 import '../input/input';
+import '../i-button/i-button';
 import '../form-item-container/form-item-container';
 import '../date-dropdown/date-dropdown';
 import '../guests-dropdown/guests-dropdown';
